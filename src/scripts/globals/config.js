815b964275aade0f0b74cb4dad54381014cf9fd7
mrsyafapri <rizkisyafapri@gmail.com>
2021-11-21 06:24:03 +0700

    first commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+  KEY: '65f062e424b16eac4fad0c9d9f7324cc',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: 'MovieCatalogue-V1',
+  DATABASE_NAME: 'movie-catalogue-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'movies',
+  WEB_SOCKET_SERVER: 'wss://stormy-badlands-06326.herokuapp.com',
+};
+
+export default CONFIG;
